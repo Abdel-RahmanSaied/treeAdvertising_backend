eb497304f58f2070048101f3e1b75bcd87ef9504
Abdel-RahmanSaied <abdo.am169@gmail.com>
2022-05-31 19:06:15 +0200

    update order complete

--- treeApp/test.py
+++ treeApp/test.py
@@ -3,30 +3,32 @@ import requests
 
 base_url = "http://127.0.0.1:8000/orders/"
 d = datetime.date(1997, 10, 19)
-data =     {
+data = {
         "recived_date": d,
         "delivery_date": d,
         "design_types": "A",
         "design_path": "dsf",
-        "design_category": [
-            "x"
-        ],
-        "printing_type": [
-            "x"
-        ],
-        "size_width": 125.0,
-        "size_high": 125.0,
-        "materials": "sdf",
-        "color": "dsf",
-        "thickness": 12.0,
-        "Post_print_services": [
-            "x"
-        ],
+        "design_category": ["x"],
+        "printing_type": ["x"],
+        "size_width": 56.0,
+        "size_high": 645.0,
+        "materials": "645",
+        "color": "gdf",
+        "thickness": 12.2,
+        "Post_print_services": ["x"],
         "state": "D",
         "notes": "asd",
-        "user_id": "saied",
-        "client_id": "zabbix"
+        "user_id": 2,
+        "client_id": 1
     }
 
-r = requests.post(base_url , data)
-print(r)
+headers = {'Accept': '*/*; indent=4', 'Content-Type': 'application/json',
+           'Authorization': 'Token 2a78f3a94598e77134f4bd9e5abe219859646fe0'}
+
+
+
+post = requests.post(base_url , data=data , headers=headers).json()
+print(post)
+
+# get = requests.get(base_url)
+# print(get.json())
